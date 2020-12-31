5a5e416abe179dbaa2ae0b93d0e1c573f4bbadb4
Jon <bedardjo@gmail.com>
2020-12-31 10:40:07 -0500

    update asset stuff

--- lib/src/views/playing-card-view.dart
+++ lib/src/views/playing-card-view.dart
@@ -90,9 +90,12 @@ class JackOfClubs extends StatelessWidget {
     return PlayingCardView(
         valueText: "J",
         valueTextStyle: TextStyle(color: Colors.black, fontSize: 24),
-        suitBuilder: (context) =>
-            Image.asset("lib/assets/card_imagery/club.png"),
-        center: (context) => Image.asset("lib/assets/card_imagery/jc.png"));
+        suitBuilder: (context) => Image.asset("assets/card_imagery/club.png",
+            package: 'playing_cards'),
+        center: (context) => Image.asset(
+              "assets/card_imagery/jc.png",
+              package: 'playing_cards',
+            ));
   }
 }
 
