dc344f670c525a17177a3c725f9e3a2f73ec37c7
Jon <bedardjo@gmail.com>
2020-12-31 10:37:37 -0500

    move assets to lib

--- lib/src/views/playing-card-view-style.dart
+++ lib/src/views/playing-card-view-style.dart
@@ -11,6 +11,18 @@ class PlayingCardViewStyle {
   final Widget Function(BuildContext context) jackOfHeartsWidget;
   final Widget Function(BuildContext context) jackOfSpadesWidget;
 
+  final Widget Function(BuildContext context) queenOfClubsWidget;
+  final Widget Function(BuildContext context) queenOfDiamondsWidget;
+  final Widget Function(BuildContext context) queenOfHeartsWidget;
+  final Widget Function(BuildContext context) queenOfSpadesWidget;
+
+  final Widget Function(BuildContext context) kingOfClubsWidget;
+  final Widget Function(BuildContext context) kingOfDiamondsWidget;
+  final Widget Function(BuildContext context) kingOfHeartsWidget;
+  final Widget Function(BuildContext context) kingOfSpadesWidget;
+
+  final Widget Function(BuildContext context) cardContentsWidget;
+
   PlayingCardViewStyle(
       {this.clubWidget,
       this.diamondWidget,
@@ -19,5 +31,14 @@ class PlayingCardViewStyle {
       this.jackOfClubsWidget,
       this.jackOfDiamondsWidget,
       this.jackOfHeartsWidget,
-      this.jackOfSpadesWidget});
+      this.jackOfSpadesWidget,
+      this.queenOfClubsWidget,
+      this.queenOfDiamondsWidget,
+      this.queenOfHeartsWidget,
+      this.queenOfSpadesWidget,
+      this.kingOfClubsWidget,
+      this.kingOfDiamondsWidget,
+      this.kingOfHeartsWidget,
+      this.kingOfSpadesWidget,
+      this.cardContentsWidget});
 }
