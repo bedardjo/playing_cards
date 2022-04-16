82e4e45bcdc822d7d4676d25d58309d065a65c3d
Matt Keller <matt@descript.com>
2022-04-16 13:21:58 -0400

    simple joker implementation

--- lib/src/model/suit.dart
+++ lib/src/model/suit.dart
@@ -1,2 +1,9 @@
-// Four standard suits.
-enum Suit { spades, hearts, diamonds, clubs }
+// The four standard suits + a special "joker" suit.
+enum Suit { spades, hearts, diamonds, clubs, joker }
+
+const STANDARD_SUITS = [
+  Suit.spades,
+  Suit.hearts,
+  Suit.diamonds,
+  Suit.clubs,
+];
