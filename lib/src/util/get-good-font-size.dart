1ac81eaa87af74e9afc65e3c3d35afeac52f4b84
Michael Gundlach <gundlach@gmail.com>
2022-09-08 22:09:54 -0400

    getGoodFontSize can size by height

--- lib/src/util/get-good-font-size.dart
+++ lib/src/util/get-good-font-size.dart
@@ -1,66 +1,54 @@
 import 'package:flutter/widgets.dart';
 
-double getGoodFontSize(String text, TextStyle style, double fitWidth) {
-  TextPainter textPainter = TextPainter(
-      text: TextSpan(text: text, style: style),
-      maxLines: 1,
-      textDirection: TextDirection.ltr);
-  textPainter.layout(minWidth: 0, maxWidth: double.infinity);
+/// Returns the largest fontSize where the given `text` takes no more than
+/// `fitSize` width (or no more than `fitSize` height, if `fitByHeight`) when
+/// using the given TextStyle (except for its fontSize).
+double getGoodFontSize(String text, TextStyle style, double fitSize,
+    {required bool fitByHeight}) {
+  double bestFontSizeSoFar = style.fontSize ?? 12;
 
-  double? minSize = style.fontSize;
-  double? maxSize = style.fontSize;
-  if (minSize == null) {
-    minSize = 12;
-  }
-  if (maxSize == null) {
-    maxSize = 12;
-  }
-
-  if (textPainter.size.width < fitWidth) {
-    while (textPainter.size.width < fitWidth) {
-      minSize = style.fontSize;
-      if (minSize == null) {
-        minSize = 12;
-      }
-      maxSize = minSize * 2;
-      style = style.copyWith(fontSize: maxSize);
-      textPainter = TextPainter(
-          text: TextSpan(text: text, style: style),
-          maxLines: 1,
-          textDirection: TextDirection.ltr);
-      textPainter.layout(minWidth: 0, maxWidth: double.infinity);
-    }
-  } else {
-    while (textPainter.size.width > fitWidth) {
-      maxSize = style.fontSize;
-      if (maxSize == null) {
-        maxSize = 12;
-      }
-      minSize = maxSize / 2;
-      style = style.copyWith(fontSize: minSize);
-      textPainter = TextPainter(
-          text: TextSpan(text: text, style: style),
-          maxLines: 1,
-          textDirection: TextDirection.ltr);
-      textPainter.layout(minWidth: 0, maxWidth: double.infinity);
-    }
-  }
-
-  // at this point, min is less and max is larger
-  while ((maxSize! - minSize!) > 3.0) {
-    double mid = minSize + (maxSize - minSize) / 2.0;
-    style = style.copyWith(fontSize: mid);
-    textPainter = TextPainter(
-        text: TextSpan(text: text, style: style),
+  // Returns the text's size (width or height) using the bestFontSizeSoFar.
+  double bestTextSizeSoFar() {
+    var bestStyleSoFar = style.copyWith(fontSize: bestFontSizeSoFar);
+    var textPainter = TextPainter(
+        text: TextSpan(text: text, style: bestStyleSoFar),
         maxLines: 1,
         textDirection: TextDirection.ltr);
     textPainter.layout(minWidth: 0, maxWidth: double.infinity);
-    if (textPainter.size.width < fitWidth) {
-      minSize = mid;
+    return fitByHeight ? textPainter.size.height : textPainter.size.width;
+  }
+
+  double? scaleBy;
+  while (true) {
+    var textSize = bestTextSizeSoFar();
+    if (textSize < fitSize) {
+      if (scaleBy == .5) break; // we were shrinking, now we're too small
+      scaleBy = 2.0; // we were at the start, or growing: keep growing
+    } else if (textSize > fitSize) {
+      if (scaleBy == 2.0) break; // we were growing, now we're too large
+      scaleBy = 0.5; // we were at the start, or shrinking: keep shrinking
     } else {
-      maxSize = mid;
+      return bestFontSizeSoFar; // perfect fit!
+    }
+    bestFontSizeSoFar *= scaleBy;
+  }
+  // We shrank/grew past fitSize.  Now binary search to find closest match
+  double minFontSize = bestFontSizeSoFar, maxFontSize = minFontSize / scaleBy!;
+  if (minFontSize > maxFontSize) {
+    double temp = minFontSize;
+    minFontSize = maxFontSize;
+    maxFontSize = temp;
+  }
+
+  // TODO: why 3 instead of 1?
+  while (maxFontSize - minFontSize > 3.0) {
+    bestFontSizeSoFar = (minFontSize + maxFontSize) / 2.0;
+    if (bestTextSizeSoFar() < fitSize) {
+      minFontSize = bestFontSizeSoFar;
+    } else {
+      maxFontSize = bestFontSizeSoFar;
     }
   }
 
-  return minSize;
+  return minFontSize; // err on the side of fitting within fitSize
 }
